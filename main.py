697c363bc7c6f6b4607a07ef2ac6dc91a5cc4985
Sherzod Turayev <asssher.uz@gmail.com>
2023-05-24 21:37:47 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+print('Hello World')
+person_name = 'Max'
+print(type(person_name))
+
+age = 30
+print(type(age))
+
+period = 3.2
+print(type(period))
+
+is_good = True
+print(type(is_good))
